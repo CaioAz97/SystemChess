136d1409df12c220fe4d039666454758418ec04d
Caio Azevedo <caioazvdo1997@gmail.com>
2023-07-09 21:27:06 -0300

    movimentos do Rei

--- src/Chess/pieces/Rei.java
+++ src/Chess/pieces/Rei.java
@@ -3,6 +3,7 @@ package Chess.pieces;
 import Chess.ChessPiece;
 import Chess.Color;
 import boardgame.Borda;
+import boardgame.Posiçao;
 
 public class Rei extends ChessPiece {
 
@@ -16,9 +17,65 @@ public class Rei extends ChessPiece {
 		return "R";
 	}
 
+	private boolean canMoves(Posiçao position) {
+	ChessPiece p = (ChessPiece)getBorda().piece(position);	
+		return p == null || p.getColor() != getColor();
+	}
+	
+	
 	@Override
 	public boolean[][] possibleMoves() {
 		boolean[][] mat = new boolean[getBorda().getRows()][getBorda().getColumns()];
+		
+		Posiçao p = new Posiçao(0, 0);
+		
+		// above 
+		p.setValues(position.getRow() -1 , position.getColumn());
+		if(getBorda().positionExists(p) && canMoves(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		//below
+		p.setValues(position.getRow() +1 , position.getColumn());
+		if(getBorda().positionExists(p) && canMoves(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		//left
+		p.setValues(position.getRow(), position.getColumn() -1);
+		if(getBorda().positionExists(p) && canMoves(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		//right
+		p.setValues(position.getRow(), position.getColumn() +1);
+		if(getBorda().positionExists(p) && canMoves(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		//NW
+		p.setValues(position.getRow()-1, position.getColumn() -1);
+		if(getBorda().positionExists(p) && canMoves(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		//NE
+		p.setValues(position.getRow()-1, position.getColumn() +1);
+		if(getBorda().positionExists(p) && canMoves(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		//SW
+		p.setValues(position.getRow()+1, position.getColumn() -1);
+		if(getBorda().positionExists(p) && canMoves(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		//SE
+		p.setValues(position.getRow()+1, position.getColumn() +1);
+		if(getBorda().positionExists(p) && canMoves(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
 		return mat;
 	}
 
